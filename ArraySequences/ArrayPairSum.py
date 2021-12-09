07a7e55490b9192b5ec374c4ab401de68c60d7d1
Abhishek Bhadauriya <abhishekbhadauriya7@gmail.com>
2021-12-09 15:02:10 +0530

    Array Pair Sum Finished

--- ArraySequences/ArrayPairSum.py
+++ ArraySequences/ArrayPairSum.py
@@ -12,3 +12,28 @@
 #
 # NOTE: FOR TESTING PURPOSES CHANGE YOUR FUNCTION SO IT OUTPUTS THE NUMBER OF PAIRS
 
+def pair_sum(arr,k):
+
+    if len(arr) < 2:
+        return "Less than two elements in array!"
+
+    #Sets for tracking
+
+    seen = set()
+    output = set()
+
+    for num in arr:
+
+        target = k - num
+
+        if target not in seen:
+            seen.add(num)
+
+        else:
+            output.add( (min(num, target), max(num, target)) )
+
+    print('\n'.join(map(str,list(output))))
+    return len(output)
+
+# Testing Method
+print(pair_sum([1,3,2,2],4))
